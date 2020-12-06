c11c52a08d851a9bafc3c8d44e51040df51909b2
oliver <owalters83@gmail.com>
2020-12-06 13:34:28 +0000

    Fingers crossed I don't need to go buy some more booze to finish this

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -11,7 +11,7 @@ cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
 ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
-DreamDaemon beestation.dmb -close -trusted -verbose -params "log-directory=ci" || EXIT_CODE=$?
+DreamDaemon nsv13.dmb -close -trusted -verbose -params "log-directory=ci" || EXIT_CODE=$?
 
 #We don't care if extools dies
 if [ $EXIT_CODE != 134 ]; then
