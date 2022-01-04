852e7588d7fcc242c3bbbcd16167deb1188d4ed8
IndusRobot <whiz_kid10@yahoo.com>
2022-01-04 14:54:16 -0700

    Fix reactor charts

--- tgui/packages/tgui/interfaces/RbmkStats.js
+++ tgui/packages/tgui/interfaces/RbmkStats.js
@@ -55,7 +55,7 @@ export const RbmkStats = (props, context) => {
             {data.coolantOutput} °C
           </ProgressBar>
         </Section>
-        <Section title="Reactor Statistics:" height="200px">
+        <Section fill title="Reactor Statistics:" height="200px">
           <Chart.Line
             fillPositionedParent
             data={powerData}
