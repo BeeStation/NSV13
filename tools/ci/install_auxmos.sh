36ebd76afd514ec1f3e631217341dca9efce17ae
covertcorvid <claughrey12@gmail.com>
2022-05-08 21:44:51 -0400

    Fix CI

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
-wget -O ~/.byond/bin/libauxmos.so "https://github.com/BeeStation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
+wget -O ~/.byond/bin/libauxmos.so "https://github.com/Putnam3145/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
 chmod +x ~/.byond/bin/libauxmos.so
 ldd ~/.byond/bin/libauxmos.so
