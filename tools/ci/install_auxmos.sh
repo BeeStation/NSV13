3d7a4f16a0e14d899dc5c8c065e2a563d61221a1
covertcorvid <17987483+covertcorvid@users.noreply.github.com>
2024-08-10 18:10:08 -0400

    maybe now it will work

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
-# NSV13 - use Putnam's fork
-wget -O ~/.byond/bin/libauxmos.so "https://github.com/Putnam3145/auxmos/releases/download/v${AUXMOS_VERSION}/libauxmos.so"
+# NSV13 - use fork
+wget -O ~/.byond/bin/libauxmos.so "https://github.com/covertcorvid/auxmos/releases/download/v${AUXMOS_VERSION}/libauxmos.so"
 chmod +x ~/.byond/bin/libauxmos.so
 ldd ~/.byond/bin/libauxmos.so
